e91b31157987daf15cd24053d8e3cdca00a3a7cd
nattapon <nattapon@eruditetech.net>
2018-07-05 16:39:24 +0700

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    const connectionOptions = {
-      "force new connection": true,
-      "reconnectionAttemps": "Infinity",
-      "timeout": 10000,
-      "transport": ["websocket"]
-    }
+  componentDidMount() {
     const socket = io.connect('http://localhost:3001')
+    this.setState({ socket })
   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
