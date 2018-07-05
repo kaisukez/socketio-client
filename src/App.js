2e89537d9ba1da1eb416ec9a707d8f7641b26eeb
= <zykigix@hotmail.com>
2018-07-05 19:59:40 +0700

    create board component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
 
+import Board from './components/Board';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boardState: Array(8).fill(Array(8).fill(false))
+    }
+  }
   componentDidMount() {
-    const socket = io.connect('http://localhost:3001')
-    this.setState({ socket })
+    const socket = io.connect('http://localhost:3001');
+    this.setState({ socket });
   }
 
   render() {
     return (
       <div className="App">
-        
+        <Board boardState={this.state.boardState} />
       </div>
     );
   }
